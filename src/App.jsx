dcfe4c41ae5eb8ede5fccdcccf6b2419da52e1ce
subhash-tiwari <pradumtiwariji@gmail.com>
2023-09-07 10:57:22 +0530

    Matches Page Made

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Matches from "./Matches";
+const App=()=>{
+    return (
+        <>
+        <Matches/>
+        </>
+    );
+}
+export default App;
---
+++ src/Matches.jsx
@@ -0,0 +1,181 @@
+import React from "react";
+
+const Matches=()=>{
+ return(
+   <>
+   <div className="container-fluid" style={{backgroundColor:"#d9d9d9",paddingBottom:"10rem"}}>
+<div className="row">
+<div className="col-12">
+<h3 className="mx-4"style={{textAlign:"left"}}>Matches</h3>
+</div>
+</div>
+<div className="row">
+<div className="col-12">
+<h3 className="mx-4"style={{textAlign:"right",color:"#9c7076"}}>Organization Name:Se7en</h3>
+</div>
+</div>
+<div className="row mt-5">
+<div className="col-sm-2">
+<h3 className="mx-4" style={{textDecoration:"underline"}}>Solo</h3>
+</div>
+<div className="col-sm-2">
+<h3 className="mx-2">duo</h3>
+</div>
+<div className="col-sm-2">
+<h3 className="mx-2">Squad</h3>
+</div>
+<div className="col-sm-2">
+<h3 className="mx-2">Quad</h3>
+</div>
+<div className="col-sm-2">
+<h3 className="mx-2">Select Games</h3>
+</div>
+<div className="col-sm-2">
+<h3 className="mx-3">Match Pass</h3>
+</div>
+</div>
+<div className="row mt-5">
+ <div className="col-sm-1"></div>
+ <div className="col-sm-3" style={{borderRadius:"0.4rem",backgroundColor:"#b4b4b4"}}>
+ <div className="row" style={{marginTop:"1rem"}}>
+
+ <div className="col-2">
+ <img src="https://cdn-icons-png.flaticon.com/128/824/824734.png" className="img-fluid" alt="real madrid"/>
+ </div>
+ <div className="col-1"></div>
+ <div className="col-6" style={{textAlign:"center"}}>
+ <h3>H2H</h3>
+ </div>
+ <div className="col-1"></div>
+ <div className="col-2">
+ <img src="https://cdn-icons-png.flaticon.com/128/824/824748.png" className="img-fluid" alt="Barcelona"/>
+ </div>
+  <hr/>
+ </div>
+ <div className="row"  style={{marginTop:"0.8"}}>
+  <div className="col-4" style={{textAlign:"left"}}>
+  <p style={{fontSize:"0.7rem",fontWeight:"bold"}}>REAL MADRID</p>
+  </div>
+  <div className="col-3"></div>
+  <div className="col-5">
+  <p style={{fontSize:"0.7rem",fontWeight:"bold",textAlign:"right"}}>BARCELONA</p>
+  </div>
+  <hr/>
+ </div>
+ <div className="row" style={{marginTop:"0rem"}}>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>245</p>
+ </div>
+ <div className="col-6" style={{textAlign:"center",fontWeight:"medium"}}>
+ <p style={{fontSize:"0.8rem"}}>Matches</p>
+ </div>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>245</p>
+ </div>
+ <hr/>
+ </div>
+ <div className="row" style={{marginTop:"0rem"}}>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>97</p>
+ </div>
+ <div className="col-6" style={{textAlign:"center",fontWeight:"medium"}}>
+ <p style={{fontSize:"0.8rem"}}>Wins</p>
+ </div>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>96</p>
+ </div>
+ <hr/>
+ </div>
+ <div className="row" style={{marginTop:"0rem"}}>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>52</p>
+ </div>
+ <div className="col-6" style={{textAlign:"center",fontWeight:"medium"}}>
+ <p style={{fontSize:"0.8rem"}}>Draws</p>
+ </div>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>52</p>
+ </div>
+ <hr/>
+ </div>
+ <div className="row" style={{marginTop:"0rem"}}>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>96</p>
+ </div>
+ <div className="col-6" style={{textAlign:"center",fontWeight:"medium"}}>
+ <p style={{fontSize:"0.8rem"}}>Loses</p>
+ </div>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>97</p>
+ </div>
+ <hr/>
+ </div>
+ <div className="row" style={{marginTop:"0rem"}}>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>1.40</p>
+ </div>
+ <div className="col-6" style={{textAlign:"center",fontWeight:"medium"}}>
+ <p style={{fontSize:"0.8rem"}}>Points per game</p>
+ </div>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>1.39</p>
+ </div>
+ <hr/>
+ </div>
+ <div className="row" style={{marginTop:"0rem"}}>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>1.67</p>
+ </div>
+ <div className="col-6" style={{textAlign:"center",fontWeight:"medium"}}>
+ <p style={{fontSize:"0.8rem"}}>Goals scored per game</p>
+ </div>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>1.63</p>
+ </div>
+ <hr/>
+ </div>
+ <div className="row" style={{marginTop:"0rem"}}>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>7</p>
+ </div>
+ <div className="col-6" style={{textAlign:"center",fontWeight:"medium"}}>
+ <p style={{fontSize:"0.8rem"}}>Longest winning streak</p>
+ </div>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>5</p>
+ </div>
+ <hr/>
+ </div>
+ <div className="row" style={{marginTop:"0rem"}}>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>11-2</p>
+ </div>
+ <div className="col-6" style={{textAlign:"center",fontWeight:"medium"}}>
+ <p style={{fontSize:"0.8rem"}}>Biggest win</p>
+ </div>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>7-2</p>
+ </div>
+ <hr/>
+ </div>
+ <div className="row" style={{marginTop:"0rem"}}>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>34</p>
+ </div>
+ <div className="col-6" style={{textAlign:"center",fontWeight:"medium"}}>
+ <p style={{fontSize:"0.8rem"}}>Lalinga trophies</p>
+ </div>
+ <div className="col-3" style={{textAlign:"center",fontWeight:"bold"}}>
+ <p>26</p>
+ </div>
+ <hr/>
+ </div>
+ </div>
+ <div className="col-8"></div>
+</div>
+</div>
+   </>
+
+ )   
+}
+export default Matches;
